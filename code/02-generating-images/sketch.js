16f65f8b6524b779c479a981cc4d2d3167b498b2
julianadenauer <github@julianadenauer.de>
2019-11-06 14:22:03 +0100

    first iteration of the drawing

--- code/02-generating-images/sketch.js
+++ code/02-generating-images/sketch.js
@@ -6,8 +6,36 @@ function setup() {
 
 function draw() {
   background("#2821AF");
-  textSize(100);
-  textFont("Typefesse Claire Obscure");
-  text("test", 50, 100);
+
+  fill(255);
+
+  textSize(12);
+  textFont("Avara Bold Italic");
+  text("Machines for Future", 20, 0, width-40, 40);
+  textAlign(CENTER);
+  text("WS 2019/2020", 20, 0, width-40, 40);
+  textAlign(RIGHT);
+  text("KH Berlin Weißensee", 20, 0, width-40, 40);
+
   
+  // select the webfont we loaded in index.html
+  textFont("Typefesse Pleine");
+  
+  textSize(mouseY);
+
+  // text horizontal und vertikal zentrieren
+  textAlign(LEFT, CENTER);
+  translate(width/2, height/2);
+
+  rotate(mouseX/100.0);
+
+  // eine kleine schleife
+  for(let i=0; i<12; i++){
+    rotate(PI/6)
+    text("#machines4future", 100, 0);
+  }
+
+  
+  
+
 }
